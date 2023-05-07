4ac779dcd3830fccb90ce013c3dd6c632c75e5a5
Hammvin <hammondvin@gmail.com>
2023-05-07 17:36:16 +0300

    Final edits and styling done

--- src/Components/NavbarReal.jsx
+++ src/Components/NavbarReal.jsx
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Logo from '../images/Logo.jpeg';
+import { Link } from 'react-router-dom';
 
 function NavbarReal() {
   return (
@@ -11,12 +12,12 @@ function NavbarReal() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="home">Home</Nav.Link>
-            <Nav.Link href="about">About NTP</Nav.Link>
-            <Nav.Link href="involve">Get Involved</Nav.Link>
-            <Nav.Link href="issues">NTP Road Map</Nav.Link>
-            <Nav.Link href="news">NTP Updates</Nav.Link>
-            <Nav.Link href="contacts">Connect With NTP</Nav.Link>
+            <Nav.Link as={Link} to={'/home'} className='navlink'>Home</Nav.Link>
+            <Nav.Link as={Link} to={'about'} className='navlink'>About NTP</Nav.Link>
+            <Nav.Link as={Link} to={'involve'} className='navlink'>Get Involved</Nav.Link>
+            <Nav.Link as={Link} to={'issues'} className='navlink'>NTP Road Map</Nav.Link>
+            <Nav.Link as={Link} to={'news'} className='navlink'>NTP Updates</Nav.Link>
+            <Nav.Link as={Link} to={'contacts'} className='navlink'>Connect With NTP</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
